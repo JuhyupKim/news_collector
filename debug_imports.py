e4fbd143e6dc0fad486e9e4251e391802d33b35d
JuhyupKim <juhyup.kim@example.com>
2026-03-26 18:06:01 +0900

    docs: 업데이트된 크롤러 최적화 및 중복 방지 기능 설명 추가
    - README.md에 MAX_ITEMS_PER_CRAWLER 설정 가이드 추가
    
    - .crawled_history.txt 를 통한 스마트 중복 방지 안내 추가

---
+++ debug_imports.py
@@ -0,0 +1,23 @@
+import pandas as pd
+print("Pandas imported")
+import os
+import sys
+import io
+import datetime
+import concurrent.futures
+print("Standard libs imported")
+import zdwang_crawler
+print("zdwang imported")
+import cheaa_crawler
+print("cheaa imported")
+import samsung_crawler
+print("samsung imported")
+import techworld_crawler
+print("techworld imported")
+import irobotnews_crawler
+print("irobotnews imported")
+import google_news_crawler
+print("google_news imported")
+import businesspost_crawler
+print("businesspost imported")
+print("All imports successful")
